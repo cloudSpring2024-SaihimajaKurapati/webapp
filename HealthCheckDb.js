211c1ca3a1f78c6b4a4b435137a4ad4013a586e8
HimajaChowdary <chowdary.ku@northeastern.edu>
2024-02-13 16:15:02 -0500

    adding config file for db

--- HealthCheckDb.js
+++ HealthCheckDb.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const User = require('./src/HealthCheck/model/user');
+const config = require('./config');
 
+const { dialect, host, username, password, database } = config.database
 const sequelize = new Sequelize( {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    dialect: dialect,
+    host: host,
+    username: username,
+    password: password,
+    database: database,
 });
 
 const userModel = User(sequelize);
---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        dialect: 'mysql',
+        host: 'localhost',
+        username: 'root',
+        password: 'Pramsai@123',
+        database: 'health_check_db',
+    }
+};
