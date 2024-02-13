dadb99417762f3394014c9633e93ae266bcb04d1
HimajaChowdary <chowdary.ku@northeastern.edu>
2024-02-13 14:39:52 -0500

    adding port

--- HealthCheckDb.js
+++ HealthCheckDb.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize( {
     host: process.env.DB_HOST,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
 });
 
