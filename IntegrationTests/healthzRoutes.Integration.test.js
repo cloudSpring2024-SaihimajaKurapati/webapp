bbf74c6ce4da3c6392b0431ab86c49b5296555a2
HimajaChowdary <chowdary.ku@northeastern.edu>
2024-02-14 13:11:39 -0500

    ading changes to gitignore

--- IntegrationTests/healthzRoutes.Integration.test.js
+++ IntegrationTests/healthzRoutes.Integration.test.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
-const app = require('../HealthCheckServer'); // Assuming your server instance is exported from 'HealthCheckServer.js'
-
+const app = require('../HealthCheckServer'); 
 describe('User Endpoint Integration Tests', () => {
 
   afterEach(() => {
